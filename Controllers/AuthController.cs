a0739557b99eed1c8db2f63ebf6a425076b3bab9
Angel Sanchez <angeljackson0933@gmail.com>
2022-06-22 10:07:15 -0500

    init project

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace JwtWebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+
+    public static User user = new User();
+
+    [HttpPost("resgister")]
+    public ActionResult<User> Register([FromBody] UserDto request)
+    {
+        CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+        user.Username = request.Username;
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+
+        return Ok(user);
+    }
+
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+}
---
+++ UserDto.cs
@@ -0,0 +1,8 @@
+namespace JwtWebApi;
+
+public class UserDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
+
